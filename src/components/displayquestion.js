9ce6ac563cf1511aff6a598c1cb09022bf472e17
shivika24 <43493690+shivika24@users.noreply.github.com>
2020-10-12 17:56:51 +0530

    Update displayquestion.js

--- src/components/displayquestion.js
+++ src/components/displayquestion.js
@@ -4,10 +4,10 @@ const Displayquestion = ({ label, ...props }) => {
     return (
         <div>
             <h1>{props.questions[0]?"Problem"+props.questions[0].sno+":- "+props.questions[0].heading:null}</h1>
-                       <p><span>{props.questions[0]?props.questions[0].question:null}</span></p>
+                <p><span>{props.questions[0]?props.questions[0].question:null}</span></p>
                       <Codingbody question="solution1"/>
-                      <h1>{props.questions[1]?"Que"+props.questions[1].sno+":- "+props.questions[1].heading:null}</h1>
-                      <p><span>{props.questions[1]?props.questions[1].question:null}</span></p>
+            <h1>{props.questions[1]?"Problem"+props.questions[1].sno+":- "+props.questions[1].heading:null}</h1>
+                <p><span>{props.questions[1]?props.questions[1].question:null}</span></p>
                       <Codingbody question="solution2"/>
         </div>  
     );
