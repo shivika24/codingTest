0cd08d8e3c98f64bec5fddbb34ce347ba8181e8e
shivika24 <43493690+shivika24@users.noreply.github.com>
2020-10-12 19:52:02 +0530

    Update messages.js

--- src/components/messages.js
+++ src/components/messages.js
@@ -4,6 +4,7 @@ let error = {
  REQUIRED               : "Required",
  ALREADY_SUBMITTED      : "Test Already Submitted From this Email-id",
  SUBMITTED_SUCCESSFULLY : "Test Submitted Successfully",
- ERROR_OCCURED          : "Some Error Occured"
+ ERROR_OCCURED          : "Some Error Occured",
+ CHARACTER_COUNT        : "Optimise Your Solution!! Solution can not exceed 2000 characters!!"
 }
 export default error;
